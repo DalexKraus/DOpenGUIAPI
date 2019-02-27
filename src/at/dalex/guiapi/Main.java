93e9e8225de28cb3f147a3bd93a3edd01c7c8b1e
DalexKraus <dalex.kraus@gmail.com>
2019-02-27 13:41:56 +0100

    Removed Test-Listener from Main

--- src/at/dalex/guiapi/Main.java
+++ src/at/dalex/guiapi/Main.java
@@ -1,21 +1,15 @@
 package at.dalex.guiapi;
 
 import at.dalex.guiapi.view.GUIManager;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerToggleSneakEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin implements Listener {
+public class Main extends JavaPlugin {
 
     private static Main instance;
-    private TestGUI gui;
 
     @Override
     public void onEnable() {
         instance = this;
-        gui = new TestGUI();
-        getServer().getPluginManager().registerEvents(this, this);
     }
 
     @Override
@@ -26,12 +20,5 @@ public class Main extends JavaPlugin implements Listener {
 
     public static Main getInstance() {
         return instance;
-    }
-
-    @EventHandler
-    public void onSneak(PlayerToggleSneakEvent e) {
-        if (e.getPlayer().isSneaking()) {
-            gui.openGUI(e.getPlayer());
-        }
     }
 }
